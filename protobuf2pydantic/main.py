402740735efd59378524da414b0154519f2cd22c
Ed-XCF <m.tofu@qq.com>
2021-01-04 21:31:12 +0800

    simplfy

--- protobuf2pydantic/main.py
+++ protobuf2pydantic/main.py
@@ -12,8 +12,7 @@ app = Typer()
 @app.command()
 def pydantic(pb2_path: str):
     path = Path(pb2_path).resolve()
-    package = path.parent
-    sys.path.append(str(package))
+    sys.path.append(str(path.parent))
     module = import_module(path.name.split('.')[0])
     py = biz.pb2_to_pydantic(module)
     return py
